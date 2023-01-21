13fcac302764a1d995439bfae008c520c236e5a0
namanjain123 <naman.nishi@gmail.com>
2023-01-21 23:28:45 +0530

    addition of new test and code for practice

---
+++ others/test_1.js
@@ -0,0 +1,7 @@
+//BMI Calculator
+
+//BMI = mass / height **2 =
+var mass = prompt("Tell Your Mass");
+var height = prompt("Tell me your height");
+var BMI=mass / (height*height);
+console.log("your bmi is",BMI);
